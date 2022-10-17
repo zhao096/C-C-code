2d8cafdaf7d153a30627e36a78bc293a06248ea9
Zhao_Y_Kun <1678200196@qq.com>
2022-10-17 19:14:24 +0800

    12

---
+++ 练习题10_17/练习题10_17/test.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d %d", &x, &y);
+//	printf("%d %d\n", x / y, x % y);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int x = 0; int y = 0;
+//	scanf("%d", &x);
+//	if (x < 0)
+//		printf("%d\n", y = 1);
+//	else if (x == 0)
+//		printf("%d\n", y = 0);
+//	else
+//		printf("%d\n", y = -1);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	scanf("%d", &i);
+	if (i % 5 == 0)
+		printf("YES\n");
+	else
+
+		printf("NO\n");
+
+	return 0;
+}
