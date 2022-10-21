3ec9d13ae9dfdedbf06a8ee226503838398d3ade
Zhao_Y_Kun <1678200196@qq.com>
2022-10-21 21:55:30 +0800

    1

---
+++ 10-21/10-21/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int c = 2;
+	if (a == 1)
+		if (c == 2)
+			printf("hehe\n");
+	else
+			printf("haha\n");
+	return 0;
+}
