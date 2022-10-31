f48877ee4124f1d0b06865811670e5f09e956ae1
Zhao_Y_Kun <1678200196@qq.com>
2022-10-31 22:26:25 +0800

    1

---
+++ 每日一练10-31/每日一练10-31/test.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int i = 0;
+//	int arr[100] = { 0 };
+//	int count = 0;
+//	int re = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//
+//		if (a == 0)
+//			break;
+//		if ((a % 10) % 2 == 0)
+//		{
+//			a = (a / 10);
+//			arr[i] = 0;
+//
+//			count++;
+//		}
+//		else
+//		{
+//			a = (a / 10);
+//			arr[i] = 1;
+//			count++;
+//		}
+//
+//	}
+//		for (i = count - 1; i >= 0; i--)
+//		{
+//			re = re * 10 + arr[i];
+//		}
+//		printf("%d", re);
+//
+//
+//	return 0;
+//}
+int i;
+
+int main()
+{
+	printf("%d", i);
+	
+	int i = 10;
+	return 0;
+}
