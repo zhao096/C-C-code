e2370666fe72911a57351932757e0bc2d7a078e5
Zhao_Y_Kun <1678200196@qq.com>
2022-11-18 22:04:18 +0800

    1

--- 冒泡排序/冒泡排序/test.c
+++ 冒泡排序/冒泡排序/test.c
@@ -6,22 +6,25 @@ int main()
 	int sz = sizeof(arr) / sizeof(arr[0]) ;
 	int i = 0;
 	int j = 0;
-	for (i = 0; i < sz - 1; i++)
+	for (i = 0; i < (sz - 1); i++)//
 	{
-		for (j = 0; j < sz - 1 - i; j++)
+		int count = 0;
+
+		for (j = 0; j < sz - 1 - i; j++)//d当你第一次没有交换时，说明后面的更大，但是j仍然会++，“这样就会使后面大的在和后面进行比较”
 		{
 			if (arr[j] >= arr[j + 1])
 			{
 				int tmp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = tmp;
+				count = 1;
 			}
-			//else
-			//{
-			//	break;
-			//}
-		}
 
+		}
+		if (count == 0)
+		{
+			break;
+		}
 	}
 
 	for (i = 0; i < sz ; i++)
