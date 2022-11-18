f00c88e97b57ede9cd6ad5b6e3a345bef43996cb
Zhao_Y_Kun <1678200196@qq.com>
2022-11-18 22:09:49 +0800

    1

--- 输入年月知该月有多少天/输入年月知该月有多少天/test.c
+++ 输入年月知该月有多少天/输入年月知该月有多少天/test.c
@@ -1,32 +1,49 @@
  #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+//int main()
+//{
+//	int y = 0;
+//	int m = 0;
+//	scanf("%d %d", &y, &m);
+//	if (m == 2)
+//	{
+//		if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
+//			printf("%d", 29);
+//		else
+//			printf("%d", 28);
+//	}
+//	else if (m <= 7)
+//	{
+//		if (m % 2 == 0)
+//			printf("%d", 30);
+//		else
+//			printf("%d", 31);
+//	}
+//	else
+//	{
+//		if (m % 2 == 0)
+//			printf("%d", 31);
+//		else
+//			printf("%d", 30);
+//	}
+//
+//	return 0;
+//}
 int main()
 {
 	int y = 0;
 	int m = 0;
-
-	scanf("%d %d", &y,&m);
-	if (m == 2)
+	while ((scanf("%d %d", &y, &m)) == 2)
 	{
-		if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
-			printf("%d", 29);
-		else
-			printf("%d", 28);
-
+		int a[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+		int day = a[m];
+		if (m == 2 && ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0)))
+		{
+			day++;
+		}
+		printf("%d\n", day);
 	}
-	else if (m <= 7)
-		if (m % 2 == 0)
-			printf("%d", 30);
-		else
-			printf("%d", 31);
-	else
-		if (m % 2 == 0)
-			printf("%d", 31);
-
-		else
-			printf("%d", 30);
-
-
+	
 
 	return 0;
 }
