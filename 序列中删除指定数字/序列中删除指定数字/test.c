7f18a815e2cb0695499c46431405849dc79669d1
Zhao_Y_Kun <1678200196@qq.com>
2022-11-19 12:10:07 +0800

    1

--- 序列中删除指定数字/序列中删除指定数字/test.c
+++ 序列中删除指定数字/序列中删除指定数字/test.c
@@ -1,32 +1,60 @@
  #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int count = 0;
+//	int arr[1000] = { 0 };
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		count++;
+//	}
+//	int del = 0;
+//
+//	int k[1000] = {-1};
+//	scanf("%d", &del);
+//	for (i = 0 ; i < n ;i++)
+//	{
+//		if (arr[i] == del)
+//		{
+//			k[i] = i;//用k来记录相等的下标i
+//		}
+//	}
+//	for(i = 0; i < count;i++)
+//	{
+//		if (i == k[i])
+//			continue;
+//		printf("%d ", arr[i]);
+//	}
+//
+//	return 0;
+//}
+
 int main()
 {
+	int i = 0;
 	int n = 0;
 	scanf("%d", &n);
-	int i = 0;
-	int count = 0;
-	int arr[1000] = { 0 };
+	int arr[10] = { 0 };
 	for (i = 0; i < n; i++)
 	{
 		scanf("%d", &arr[i]);
-		count++;
 	}
+	int j = 0;
 	int del = 0;
-
-	int k[1000] = {-1};
 	scanf("%d", &del);
-	for (i = 0 ; i < n ;i++)
+	for (i = 0; i < n; i++)
 	{
-		if (arr[i] == del)
+		if (arr[i] != del)
 		{
-			k[i] = i;//用k来记录相等的下标i
+			arr[j++] = arr[i];//arr[j] 与 arr[i]同步 ，当arr[i] = del 时就不进入改变arr[j]也就是不存该数，然后若不是del则会存进去
 		}
 	}
-	for(i = 0; i < count;i++)
+	for (i = 0; i < j; i++)
 	{
-		if (i == k[i])
-			continue;
 		printf("%d ", arr[i]);
 	}
 
