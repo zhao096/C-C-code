06b90dd1c45a012b3fcec656e518aec8e1586661
Zhao_Y_Kun <1678200196@qq.com>
2022-11-22 20:37:51 +0800

    1

---
+++ 使用指针打印数组内容/使用指针打印数组内容/test.c
@@ -0,0 +1,23 @@
+ #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void print(int* arr,int n)
+{
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(arr + i));
+	}
+}
+
+int main()
+{
+	int arr[10] = { 0 };
+	int sz = sizeof(arr) / sizeof(arr[1]);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	print(arr,sz);
+	return 0;
+}
