88e9fd31632aa253210913031fd3cf63a6025e74
Zhao_Y_Kun <1678200196@qq.com>
2022-11-28 00:01:38 +0800

    1

--- strlen的实现/strlen的实现/test.c
+++ strlen的实现/strlen的实现/test.c
@@ -2,19 +2,28 @@
 #include<assert.h>
 #include<stdio.h>
 
-int my_strlen(const char* string)
+//int my_strlen(const char* string)
+//{
+//	assert(string);//防止string == NULL
+//	int count = 0;
+//	while( *string++ != '\0')
+//	{	
+//		count++;
+//	}
+//	return count;
+//}
+size_t my_strlen(const char* string)
 {
-	assert(string);
-	int count = 0;
-	while( *string++ != '\0')
-	{	
-		count++;
+	const char* star = string;//若没在最开始加const 就会报错，因为string也是const
+	while (*string++)//这样写他在判断成string为0后仍会加1
+	{
+		;
 	}
-	return count;
+	return string - star -1;//所以要减1
 }
 int main()
 {
 	char arr[] = "abcdef";
-	printf("%d", my_strlen(arr));
+	printf("%d", my_strlen(arr));//链式访问，他会返回字符个数
 	return 0;
 }
