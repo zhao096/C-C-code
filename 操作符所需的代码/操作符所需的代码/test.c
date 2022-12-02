a4bfa1558e806ae88d69fa3b37495cc87283b4ea
Zhao_Y_Kun <1678200196@qq.com>
2022-12-02 21:25:45 +0800

    1

--- 操作符所需的代码/操作符所需的代码/test.c
+++ 操作符所需的代码/操作符所需的代码/test.c
@@ -76,12 +76,20 @@
 //    return 0;
 //}
 
+//int main()
+//{
+//	int a = 0;
+//	int b = 10;
+//	int c = a + b;
+//	return 0;
+//}
+#include <stdio.h>
 int main()
 {
-	int a = 0;
-	int b = 10;
-	int c = a + b;
-	return 0;
+    int i = 0, a = 0, b = 2, c = 3, d = 4;
+    i = a++ && ++b && d++;
+    printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+    return 0;
 }
 
 
