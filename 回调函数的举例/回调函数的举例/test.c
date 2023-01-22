f06932c570a7a225912e83963f17dd35cb59e0f3
Zhao_Y_Kun <1678200196@qq.com>
2023-01-22 22:45:05 +0800

    1

---
+++ 回调函数的举例/回调函数的举例/test.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void menu()
+{
+	printf("********************************\n");
+	printf("**********1.Add   2.Sub*********\n");
+	printf("**********3.Mul   4.Div*********\n");
+	printf("**********   0.exit    *********\n");
+	printf("********************************\n");
+}
+
+int Add(int x, int y)
+{
+	return  x + y;
+}
+
+int Sub(int x, int y)
+{
+	return  x - y;
+}
+
+int Mul(int x, int y)
+{
+	return  x * y;
+}
+
+int Div(int x, int y)
+{
+	return  x / y;
+}
+
+
+void calc(int (*pf) (int, int))
+{
+	int a = 0;
+	int b = 0;
+	printf("请输入两个值:");
+	scanf("%d %d", &a, &b);
+	printf("%d\n", pf(a, b));
+}
+
+
+int main()
+{
+	int input = 0;
+	int a = 0;
+	int b = 0;
+	do
+	{
+		menu();
+		printf("请输入：");
+		scanf("%d", &input);
+		switch (input)
+		{ 
+		case 1:
+			calc(Add);
+			break;
+		case 2:
+			calc(Sub);
+
+			break;
+		case 3:
+			calc(Mul);
+
+			break;
+		case 4:
+			calc(Div);
+
+			break;
+		case 0:
+			printf("退出计算器\n");
+			break;
+		default:
+			printf("输入错误请重新输入\n");
+			break;
+		}
+
+	} while (input);
+
+	return 0;
+}
