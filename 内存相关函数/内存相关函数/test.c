e48b624e0eef5255944e7c3c1111f0fdba695307
Zhao_Y_Kun <1678200196@qq.com>
2023-02-14 23:55:58 +0800

    1

--- 内存相关函数/内存相关函数/test.c
+++ 内存相关函数/内存相关函数/test.c
@@ -1,1 +1,25 @@
  #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//    int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+//    int arr1[10] = { 0 };
+//    memcpy(arr1, arr, 20);//¿½±´5¸öint 4 * 5 = 20 byte
+//    for(int i = 0; i<10 ; i++)
+//    printf("%d ", arr1[i]);
+//    return 0;
+//
+//}
+int main()
+{
+	int arr1[] = { 1,4,7 };
+
+	int arr2[] = { 1,4,2 };
+
+	int ret = memcmp(arr1, arr2,sizeof(arr1));
+
+	printf("%d", ret);
+
+	return 0;
+}
