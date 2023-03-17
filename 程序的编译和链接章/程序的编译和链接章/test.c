26569c4ed42f5cb0fa351da4946e37f9799efb9f
Zhao_Y_Kun <1678200196@qq.com>
2023-03-17 23:53:59 +0800

    1

--- 程序的编译和链接章/程序的编译和链接章/test.c
+++ 程序的编译和链接章/程序的编译和链接章/test.c
@@ -15,12 +15,71 @@
 //	return 0;
 //}
 #define ADD(a) a + a
+//
+//int main()
+//{
+//	int a = 5;
+//	printf("%d\n", ADD(a));
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    printf("%d", ADD(3) * ADD(2));
+//    return 0;
+//}
+
+//#define PRINT(format,x) printf("the value of "#x" is "format"\n",x)
+//
+//
+//int main()
+//{
+//	int a = 10;
+//	PRINT("%d", a);
+//
+//	float f = 20.22f; //加上f是因为20.22一般会被默认成double类型的加上了就会看成float类型)
+//	PRINT("%f", f);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("abcdef\n");
+//	printf("abc""def\n");
+//	return 0;
+//}
+//
+//
+
+//#define joint(a,b) a##b
+//
+//int main()
+//{
+//	int AB = 100;
+//	
+//	printf("%s\n", joint(A, B));//最终传进去的参数会通过##拼接在一起
+//	return 0;
+//}
+
+#define MAX(a,b) ((a)>(b)?(a):(b))
 
 int main()
 {
-	int a = 5;
-	printf("%d\n", ADD(a));
+	int a = 3, b = 4;
+#undef MAX
+	int m = MAX(a++, b++);
+	//((a++) > (b++) ? (a++) : (b++))
+	//因为是a++ 、 b++ 所以先比较后++ 所以较大值4 ，然后：a = 4 b = 5；
+	//(b++)再因为还要到后面的b++ b = 6
+	printf("%d\n", m);
+	printf("%d %d", a, b);
 
 	return 0;
 }
+
+
+
 
