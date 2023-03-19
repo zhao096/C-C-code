3a8f7568b535a0d0d587acc9498e47fa40443405
Zhao_Y_Kun <1678200196@qq.com>
2023-03-19 23:56:24 +0800

    1

---
+++ 堆/堆/stack.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<stdbool.h>
+
+typedef int datetype;
+
+typedef struct stack
+{
+	datetype* date;
+	int top;
+	int capacity;
+}stack;
+
+void StackInit(stack* ps);
+void StackDestroy(stack* ps);
+
+
+void StackPush(stack* ,datetype x);
+void StackPop(stack* ps);
+//获取堆顶的数据
+datetype StackTop(stack* ps);
+//获取堆上的个数
+int StackSize(stack* ps);
+
+bool StackEmpty(stack* ps);
+
+
+
+
+
+
