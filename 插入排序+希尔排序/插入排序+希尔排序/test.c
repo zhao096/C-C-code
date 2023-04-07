b1cf9b9aca33df5aecbfa89107eab762c4c776ef
Zhao_Y_Kun <1678200196@qq.com>
2023-04-07 23:54:28 +0800

    1

--- 插入排序+希尔排序/插入排序+希尔排序/test.c
+++ 插入排序+希尔排序/插入排序+希尔排序/test.c
@@ -33,11 +33,11 @@ void Swap(int* t1, int* t2)
 
 void InsertSort(int* a, int n)
 {
-	for (int i = 0; i < n - 1; i++)
+	for (int i = 1; i < n ; i++)
 	{
-		int end = i;
-		int tmp = a[i + 1];
-		while (end >= 0)
+		int end = i-1;
+		int tmp = a[i];
+		while (end >= 0)	
 		{
 			if (tmp < a[end])
 			{
@@ -60,14 +60,41 @@ void InsertSort(int* a, int n)
 //把相差为gap的看成一组，所以一组就会有gap个元素，然后对每一组进行排序（所以他们就相差gap）
 void ShellSort(int* a, int n)
 {
-	int gap = 3;
+	//int gap = 3;
+	//for (int j = 0; j < gap; j++)
+	//{
+	//	//一次插入排序，其是其插入排序时当gap = 1时
+	//	//i < 倒数第二个元素的位置 其中第一组的倒数第二个位置已经是最大的一个了所以直接：
+	//	// i < n - gap即可
+	//	//对每组进行排序 从下标为j处开始 
+	//	for (int i = j; i < n - gap ; i += gap)
+	//	{
+	//		int end = i ;
+	//		int tmp = a[i+ gap];
+	//		while (end >= 0)
+	//		{
+	//			if (tmp < a[end])
+	//			{
+	//				Swap(&a[end + gap], &a[end]);
+	//				end-= gap;
+	//			}
+	//			else
+	//			{
+	//				break;
+	//			}
+	//		}
+	//		a[end + gap] = tmp;
+	//	}
 
-	for (int j = 0; j < gap; j++)
+	//}
+	//InsertSort(a, n);
+	int gap = n;
+	while (gap > 1)
 	{
-		//一次插入排序，其是其插入排序时当gap = 1时
-	//i < 倒数第二个元素的位置
-	// i < (n - gap + j ) % (n - 1)
-		for (int i = j; i < (n - gap + j) % (n - 1); i += gap)
+		gap /= 2;//让其不断7预排序只到gap = 1时就能排好（等于1时相当于插入排序）
+		//gap = gap / 3 + 1;
+		//直接将多组分开进行合并为一组同时进行
+		for (int i = 0; i < n - gap; i++)
 		{
 			int end = i;
 			int tmp = a[i + gap];
@@ -76,7 +103,7 @@ void ShellSort(int* a, int n)
 				if (tmp < a[end])
 				{
 					Swap(&a[end + gap], &a[end]);
-					end-= gap;
+					end -= gap;
 				}
 				else
 				{
@@ -85,27 +112,33 @@ void ShellSort(int* a, int n)
 			}
 			a[end + gap] = tmp;
 		}
-
 	}
-	//InsertSort(a, n);
+	
+
 }
 
 
 int main()
 {
-	int arr[10] = { 10,9,8,7,6,5,4,3,2,1 };
+	int arr[10] = { 0 };
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
 
-
-
-	//InsertSort(arr,10);
+	InsertSort(arr,5);
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	//ShellSort(arr, 10);
 	ShellSort(arr, 10);
 
 	for (int i = 0; i < 10; i++)
 	{
 		printf("%d ", arr[i]);
 	}
-	//ShellSort(arr, 10);
-
 
 	return 0;
 }
