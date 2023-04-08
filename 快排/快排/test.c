ae6af9316a6f135cb44d09174da04b311bad486a
Zhao_Y_Kun <1678200196@qq.com>
2023-04-08 23:56:11 +0800

    1

--- 快排/快排/test.c
+++ 快排/快排/test.c
@@ -1,69 +1,187 @@
- #define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 
+// 快速排序递归实现
+// 快速排序hoare版本
 
-//挖坑法
-void QuickSort1(int* a, int left, int right)
+//先确定一个key 通过这个key来进行分隔
+//将一个数组通过key 将小于key的放在其左边 ， 反之放到右边
+//用left找大的的、right找小的，找到后交换，然后继续找直到left和right相遇
+//让后通过改变left 和 right 来递归实现
+int PartSort1(int* a, int left, int right)
 {
 	if (left >= right)
-		return;
+	{
+		return 0;
+	}
 
-	int begin = left, end = right;
+	//生成随机的keyi
+	int r = left + (rand() % (right - left));
+	Swap(&a[left], &a[r]);
+	int keyi = left;
 
-	//// 随机选key
-	/*int randi = left + (rand() % (right - left));
-	Swap(&a[left], &a[randi]);*/
-
-	// 三数取中
+	//三数取中
 	//int midi = GetMidNumi(a, left, right);
-	//if (midi != left)
-	//	Swap(&a[midi], &a[left]);
+	//if(left != midi)
+	//	Swap(&a[left], &a[midi]);
+	//int keyi = left;//此处直接把keyi赋值为left
 
-	int tmp = a[left];
-	
-	int hole = left;
+	int end = right, begin = left;
 	while (left < right)
 	{
-		while (left < right && a[right] >= tmp)
-		{
-			--right;
-		}
-		a[hole] = a[right];
-		hole = right;
+		//while (end)//让right先找这样就能保证最终于key交换的一定是小的（key 在最左边时）
+		//{
+		//	if (a[end--] <= a[keyi])
+		//		break;
+		//}
+		//while (begin)
+		//{
+		//	if (a[begin++] > a[keyi])
+		//		break;
+		//}
+		while (right > left && a[right] >= a[keyi])//从右边开始找到小于key的
+			right--;
+		while (left < right && a[left] <= a[keyi])//从左边开始找到大于key的
+			left++;
 
-		while (left < right && a[left] <= tmp)
-		{
-			++left;
-		}
-		a[hole] = a[left];
-		hole = left;
-		
+		Swap(&a[right], &a[left]);
 	}
-	a[hole] = tmp;//把坑的位置放好原本拿出去的值
+	Swap(&a[keyi], &a[right]);
 
-	// [begin, keyi-1] keyi [keyi+1, end] 
-	// 递归
-	QuickSort1(a, begin, hole - 1);
-	QuickSort1(a, hole + 1, end);
+	keyi = right;
+
+	return keyi;
+
 }
+// 快速排序挖坑法
 
-
-
-// 前后指针法
-int PartSort3(int* a, int left, int right)
+int PartSort2(int* a, int left, int right)
 {
-	int prev = left;
-	int cur = left + 1;
+	if (left >= right)
+	{
+		return 0;
+	}
+	//将keyi所指的数据先拿出来
+	//再从左右两边来找先从右边开始找到小的就填到这个拿出来的位置hole上
+	//并且把hole改成右边下的位置的下标
+	//再从左边找大的，填到hole里
+	//重复 直到 left 和 right 相遇
+	//最后把拿出去的值放到这个坑即可
+
+	//随机选k
+	int r = left + (rand() % (right - left));
+	Swap(&a[left], &a[r]);
+
+	//三数取中
+//int midi = GetMidNumi(a, left, right);
+//if (left != midi)
+//	Swap(&a[left], &a[midi]);
+
+	int hole = left;
 
 	int keyi = a[left];
-
-	while(left <= right)
+	int begin = left, end = right;
+	while (left < right)
 	{
-		if (a[right] < keyi)
+		while (right > left && a[right] >= keyi)
 		{
-			prev++;
-			Swap(&a[prev], &a[cur]);
+			right--;
+		}
+		Swap(&a[hole], &a[right]);
+		hole = right;
+
+		while (left < right && a[left] <= keyi)
+		{
+			left++;
+		}
+		Swap(&a[hole], &a[left]);
+		hole = left;
+	}
+	a[hole] = keyi;
+
+	return hole;
+}
+// 快速排序前后指针法
+//一前一后的指针
+//cur 不断的往前走，cur如果遇到小于key的值就先让prev++ ，再交换prev和cur所指的值
+int PartSort3(int* a, int left, int right)
+{
+	if (left >= right)
+	{
+		return 0;
+	}
+
+	//随机选k
+	//int r = left + (rand() % (right - left));
+	//Swap(&a[left], &a[r]);
+	//int keyi = left;
+
+	//三数取中
+	int midi = GetMidNumi(a, left, right);
+	if (left != midi)
+		Swap(&a[left], &a[midi]);
+	int keyi = left;//此处直接把keyi赋值为left
+
+	int prev = left, cur = left + 1;
+
+	//通过两个指针进行交换把大的往后移把小的放在前面
+	while (cur <= right)
+	{
+		if (a[cur] < a[keyi] && ++prev != cur)
+		{
+			Swap(&a[cur], &a[prev]);
 		}
 		cur++;
 	}
-	Swap(&a[prev], &a[left]);
+	Swap(&a[prev], &a[keyi]);
+	keyi = prev;
+
+	return keyi;
+
+}
+void quicksort(int* a, int left, int right)
+{
+	if (left >= right)
+	{
+		return;
+	}
+	int end = right, begin = left;
+
+	int keyi = PartSort3(a, begin, end);
+	quicksort(a, begin, keyi - 1);
+	quicksort(a, keyi + 1, end);
+
+}
+
+// 快速排序 非递归实现
+void QuickSortNonR(int* a, int left, int right)
+{
+
+}
+
+int main()
+{
+	srand((unsigned int)time(0));
+	int arr[10] = { 0 };
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	//BubbleSort(arr, 5);
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	printf("%d ", arr[i]);
+	//}
+	//printf("\n");
+
+	quicksort(arr, 0, 9);
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
 }
