066bfca419f1c5eec3909dbf673ffec34953b078
Zhao_Y_Kun <1678200196@qq.com>
2023-04-24 12:51:07 +0800

    1

--- 类和对象（上）/类和对象（上）/test.cpp
+++ 类和对象（上）/类和对象（上）/test.cpp
@@ -7,65 +7,120 @@
 
 using namespace std;
 
-struct Stack
+//struct Stack
+//{
+//	//成员函数
+//	//在类域中不管声明的位置都能找得到
+//	void Init(int DefaultCapacity = 4)
+//	{
+//		a = (int*)malloc(sizeof(int) * DefaultCapacity);
+//		if (a == nullptr)
+//		{
+//			perror("malloc");
+//			return;
+//		}
+//		capacity = DefaultCapacity;
+//		size = 0;
+//	}
+//	void Push(int x)
+//	{	
+//		if (size == capacity)
+//		{
+//			int* ptr = (int*)realloc(a, sizeof(int) * capacity * 2);
+//			assert(ptr);
+//			a = ptr;
+//			capacity *= 2;
+//		}	
+//		a[size++] = x;
+//	}
+//
+//	int Top()
+//	{
+//		assert(size);
+//		return a[size - 1];
+//	}
+//
+//
+//	void Destroy()
+//	{
+//		free(a);
+//		a = nullptr;
+//		capacity = size = 0;
+//	}
+//	//成员变量
+//	int* a;
+//	int size;
+//	int capacity;
+//};
+//
+//int main()
+//{
+//	//Stack sk;
+//	//sk.Init(20); 
+//	//sk.Push(1);
+//	//sk.Push(2);
+//	//sk.Push(3);
+//	//cout << sk.Top() << endl;
+//	//sk.Destroy();
+//
+//	//Person p;
+//	//p.PersonInit();
+//	//char a[] = "LiHua";
+//	//p.ShowInfo(a);
+//	//return 0;
+//}
+
+
+//class A
+//{
+//	void fun()
+//	{
+//	}
+//};
+//class B
+//{
+//};
+//
+//int main()
+//{
+//
+//
+//	cout << sizeof(A) << endl;
+//	cout << sizeof(B) << endl;
+//	return 0;
+//}
+
+
+
+//写一个日期类的类
+class Date
 {
-	//成员函数
-	//在类域中不管声明的位置都能找得到
-	void Init(int DefaultCapacity = 4)
+public:
+	void Init(int year = 1970, int month = 1, int day = 1)
 	{
-		a = (int*)malloc(sizeof(int) * DefaultCapacity);
-		if (a == nullptr)
-		{
-			perror("malloc");
-			return;
-		}
-		capacity = DefaultCapacity;
-		size = 0;
+		_day = day;
+		_month = month;
+		_year = year;
 	}
-	void Push(int x)
-	{	
-		if (size == capacity)
-		{
-			int* ptr = (int*)realloc(a, sizeof(int) * capacity * 2);
-			assert(ptr);
-			a = ptr;
-			capacity *= 2;
-		}	
-		a[size++] = x;
-	}
-
-	int Top()
+	void Print()
 	{
-		assert(size);
-		return a[size - 1];
+		cout << _year << ' ' << _month << ' ' << _day << endl;
 	}
-
-
-	void Destroy()
-	{
-		free(a);
-		a = nullptr;
-		capacity = size = 0;
-	}
-	//成员变量
-	int* a;
-	int size;
-	int capacity;
+private:
+	int _day;
+	int _month;
+	int _year;
 };
+
 
 int main()
 {
-	Stack sk;
-	sk.Init(20); 
-	sk.Push(1);
-	sk.Push(2);
-	sk.Push(3);
-	cout << sk.Top() << endl;
-	sk.Destroy();
+	Date d1;
+	Date d2;
+	d1.Init(2004, 9, 7);
+	d2.Init(2003, 7, 7);
+	d1.Print();
+	d2.Print();
 
-	Person p;
-	p.PersonInit();
-	char a[] = "LiHua";
-	p.ShowInfo(a);
 	return 0;
 }
