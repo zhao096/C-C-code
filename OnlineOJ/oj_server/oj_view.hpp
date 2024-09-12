8d50f8e5f9465f67596b6ab4009632a234b02c63
userName <userEmail>
2024-09-12 22:23:50 +0800

    oj_view

--- OnlineOJ/oj_server/oj_view.hpp
+++ OnlineOJ/oj_server/oj_view.hpp
@@ -19,7 +19,7 @@ namespace ns_view
 
         void AllExpandHtml(std::vector<Question>& questions,std::string* html)
         {
-            std::string src_html = template_path + "all_questions";
+            std::string src_html = template_path + "all_questions.html";
             ctemplate::TemplateDictionary root(all_questions);
 
             for(auto& q : questions)
@@ -30,14 +30,26 @@ namespace ns_view
                 sub.SetValue("star",q.star);
             }
             //渲染
-            ctemplate::Template *tpl =  ctemplate::Template::GetTemplate(html,ctemplate::DO_NOT_STRIP);
+            ctemplate::Template *tpl =  ctemplate::Template::GetTemplate(src_html,ctemplate::DO_NOT_STRIP);
 
+//将旧的src_html替换成新的渲染后的tpl的html
             tpl->Expand(html,&root);
         }
 
         void OneExpandHtml(Question& q,std::string* html)
         {
+            std::string src_html = template_path + "one_question.html";
+            ctemplate::TemplateDirctory root(question);
 
+            root.SetValue("tille",q.title);
+            root.SetValue("number",q.number);
+            root.SetValue("star",q.star);
+            root.SetValue("desc",q.desc);
+            root.SetValue("pre_code",q.header);
+
+            ctemplate::Template* tpl =  ctemplate::Template::GetTemplate(src_html,ctemplate::Do_NOT_STRIP);
+
+            tpl->Expand(html,&root);
         }
 
     };
